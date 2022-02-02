5eeff85c258e27b7b10aa14c6c6e09a44c192983
Marc <marc.maninang@gmail.com>
2022-02-01 18:27:25 -0800

    updated with further exploration

--- exercises/easy1/how_big_is_the_room.js
+++ exercises/easy1/how_big_is_the_room.js
@@ -6,15 +6,26 @@ function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
-prompt('Enter the length of the room in meters: ');
+prompt('Are your measurements in meters or feet? ');
+let measurement = readline.question();
+
+prompt('Enter the length of the room: ');
 let length = readline.question();
 length = parseInt(length, 10);
 
-prompt('Enter the width of the room in meters: ');
+prompt('Enter the width of the room: ');
 let width = readline.question();
 width = parseInt(width, 10);
 
-let areaInMeters = (length * width);
-let areaInFeet = (areaInMeters * SQMETERS_TO_SQFEET);
+let areaInMeters;
+let areaInFeet;
+
+if( measurement === 'feet') {
+  areaInFeet = (length * width);
+  areaInMeters = (areaInFeet / SQMETERS_TO_SQFEET);
+} else {
+  areaInMeters = (length * width);
+  areaInFeet = (areaInMeters * SQMETERS_TO_SQFEET);
+}
 
 prompt(`The area of the room is ${areaInMeters.toFixed(2)} square meters (${areaInFeet.toFixed(2)} square feet).`);
