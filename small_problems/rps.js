95b1ba6bc72a220a52c8230424961015488a1a2c
Marc <marc.maninang@gmail.com>
2022-02-20 10:38:19 -0800

    added shortened values

--- small_problems/rps.js
+++ small_problems/rps.js
@@ -6,42 +6,80 @@
  * the computer will choose their move
  * display who won/the result
  * 
+ * 
+ * RPS Bonus Features
+ * 1. Lizard Spck
+ * 2. Shortened Input
+ * 3. Best of 5
+ * 4. ESLint Complaints
+ * 5. Ask for code review
+ * 
  */
 
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+// const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+const VALID_CHOICES = {
+  r: 'rock', 
+  p: 'paper', 
+  s: 'scissors', 
+  l: 'lizard', 
+  v: 'spock'
+}
+const CHOICE_KEYS = Object.keys(VALID_CHOICES);
+const CHOICE_VALUES = Object.values(VALID_CHOICES);
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
 }
-
+/**
+ * possible player choices
+ * rock
+ * scissors
+ * paper
+ * lizard
+ * spock
+ * 
+ * possible computer choices
+ * 
+ * scissors || lizard
+ * lizard || paper
+ * rock || spock
+ * paper || spock
+ * scissors || rock
+ * 
+ */
 function displayWinner(choice, computerChoice) {
-  prompt(`You chose ${choice}. Computer chose ${computerChoice}`);
+  prompt(`You chose ${VALID_CHOICES[choice]}. Computer chose ${computerChoice}`);
   
-  if ((choice === 'rock' && computerChoice === 'scissors') || 
-      (choice === 'scissors' && computerChoice === 'paper') || 
-      (choice === 'paper' && computerChoice === 'rock')) {
-        prompt(`You win!`);
-  } else if ((choice === 'scissors' && computerChoice === 'rock') || 
-      (choice === 'paper' && computerChoice === 'scissors') || 
-      (choice === 'rock' && computerChoice === 'paper')) {
-        prompt('Computer won!');
-      } else {
-        prompt("It's a tie!");
+  if (((choice === 'rock' || choice === 'r') && (computerChoice === 'scissors' || computerChoice === 'lizard')) || 
+      ((choice === 'scissors' || choice === 's') && (computerChoice === 'lizard' || computerChoice === 'paper')) || 
+      ((choice === 'paper' || choice === 'p') && (computerChoice === 'rock' || computerChoice === 'spock')) || 
+      ((choice === 'lizard' || choice === 'l') && (computerChoice === 'paper' || computerChoice === 'spock')) ||
+      ((choice === 'spock' || choice === 'v') && (computerChoice === 'scissors' || computerChoice === 'rock'))) {
+    prompt(`You win!`);
+  } else if (((choice === 'paper' || choice === 'p') && (computerChoice === 'scissors' || computerChoice === 'lizard')) || 
+      ((choice === 'rock' || choice === 'r') && (computerChoice === 'paper' || computerChoice === 'spock')) || 
+      ((choice === 'scissors' || choice === 's') && (computerChoice === 'rock' || computerChoice === 'spock')) || 
+      ((choice === 'spock' || choice === 'v') && (computerChoice === 'lizard' || computerChoice === 'paper')) ||
+      ((choice === 'lizard' || choice === 'l') && (computerChoice === 'scissors' || computerChoice === 'rock'))) {
+    prompt('Computer won!');
+  } else {
+    prompt("It's a tie!");
   }
+
 }
 
 while (true) {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  prompt(`Choose one: ${CHOICE_VALUES.join(', ')} or ${CHOICE_KEYS.join(', ')} respectively.`);
   let choice = readline.question();
   
-  while(!VALID_CHOICES.includes(choice)) {
+  while(!CHOICE_VALUES.includes(choice) && !CHOICE_KEYS.includes(choice)) {
     prompt("That's not a valid choice.");
     choice = readline.question();
   }
-  
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+
+  let randomIndex = Math.floor(Math.random() * CHOICE_VALUES.length);
+  let computerChoice = CHOICE_VALUES[randomIndex];
   
   displayWinner(choice, computerChoice);
 
@@ -54,19 +92,3 @@ while (true) {
 
   if (answer[0] !== 'y') break;
 }
-
-/**
- * Things to consider:
- * 
- * 1. If you move displayWinner above the prompt function it still works. 
- * This is because the functions are initiated and stored before they're used in the while loop.
- * 
- * 2. If it returned the string then I would need to store the result of the function call into
- * a variable and console log that string.
- * 
- * 3. if Math.round() was used then the function would need to be subtracted by 1. This is because
- * Math.round() rounds up, and would give a result of 1-3 instead of the array index 0-2. This 
- * goes for Math.ceil() as well.
- * 
- * 4. To stop the loop without a break statement, we can use answer[0] === 'n' as the while argument
- */
