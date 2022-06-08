1e231fd24e495624448440d7e9f20a910e383ff5
Marc <marc.maninang@gmail.com>
2022-06-07 21:50:37 -0700

    finally finished the letter swap exercise. Pretty proud that I figured it out in about an hour after such a long break from coding

--- exercises/easy4/letter_swap.js
+++ exercises/easy4/letter_swap.js
@@ -8,33 +8,38 @@
  * swap the first and last letters of every word
  * 
  * ALGORITHM
- * create newArr using .split() on string of words
- * iterate through that array
- *  - forEach word, use .split() again
- *  - iterate through each of those arrays
- *    - create variable for firstLetter, assign it word.shift()
- *    - create variable for lastLetter, assign it word.pop();
- *      - .unshift lastLetter to split word array
- *      - .push firstLetter to split word array
- *    - return the word .joined
- *  - return that word .joined
- * return new word
+ * set output variable
+ * split up string into an array to iterate over it
+ * use for loop to iterate over each word
+ * flip first letter and last letter of the word
+ *  split that word into another array to get each letter
+ *  iterate over that array
+ *  swap the position of the first letter and the last letter
+ *    use the index 0 for first letter
+ *    use index of word.length - 1 for last letter
+ *    rejoin the the letters into the word
+ *  rejoin the word into the string
+ * return result
  * 
  * 
  */
 
 function swap(string) {
+  let output = '';
   let words = string.split(' ');
-  let flipped = words.map( word => {
-    let letters = word.split('');
-    let firstLetter = letters.shift();
-    let lastLetter  = letters.pop();
-    
-    letters = 
-  });
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 1) {
+      let firstLetter = words[i].charAt(0);
+      let lastLetter = words[i].charAt(words[i].length-1);
+      let trimmedWord = words[i].substring(1, words[i].length - 1);
+      output += lastLetter + trimmedWord + firstLetter + ' ';
+    } else {
+      output += words[i] + ' ';
+    }
+  }
+  return output;
 }
 
-
-swap('Oh what a wonderful day it is');  // "hO thaw a londerfuw yad ti si"
-swap('Abcde');                          // "ebcdA"
-swap('a');                              // "a"
+console.log(swap('Oh what a wonderful day it is')); // "hO thaw a londerfuw yad ti si"
+console.log(swap('Abcde')); // "ebcdA"
+console.log(swap('a'));  // "a"
