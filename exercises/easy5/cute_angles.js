b688186d0249e39e51b64da5abe0c7e7e7e3fd9c
Marc <marc.maninang@gmail.com>
2022-06-08 23:23:48 -0700

    This one was hard. I peeked at the solution a lot to help solve the mathematical portion

---
+++ exercises/easy5/cute_angles.js
@@ -0,0 +1,45 @@
+/**
+ * PEDAC
+ * 
+ * INPUT
+ * floating point number representing an angle between 0 and 360 degrees
+ * 
+ * OUTPUT
+ * string representing that angle in degrees, minutes, and seconds
+ * degrees = ˚
+ * minutes = '
+ * seconds = "
+ * 
+ * ALGORITHM
+ * 60 minutes in a degree
+ * 60 seconds in a minute
+ * all numbers before the decimal (0 - 360) is a whole degree so subtract that from the input
+ * multiply the remainder by 60 to get the minutes
+ * multiple the remainder of minutes by 6 to get the seconds
+ * 
+ * 
+ * 
+ */
+const DEGREE = '\xB0';
+const MINUTES_PER_DEGREE = 60; 
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE;
+
+function dms(degreesFloat) {
+  let degreesInt = Math.floor(degreesFloat);
+  let minutes = Math.floor((degreesFloat - degreesInt) * MINUTES_PER_DEGREE);
+  let seconds = Math.floor((degreesFloat - degreesInt - (minutes / MINUTES_PER_DEGREE)) * SECONDS_PER_DEGREE);
+  return String(degreesInt) + DEGREE + padZeroes(minutes) + "'" + padZeroes(seconds) + '"';
+}
+
+function padZeroes(number) {
+  let numString = String(number);
+  return numString.length < 2 ? ('0' + numString) : numString;
+}
+
+console.log(dms(30));           // 30°00'00"
+console.log(dms(76.73));        // 76°43'48"
+console.log(dms(254.6));        // 254°35'59"
+console.log(dms(93.034773));    // 93°02'05"
+console.log(dms(0));            // 0°00'00"
+console.log(dms(360));          // 360°00'00" or 0°00'00"
