b1b45a7fd0121d3e17bbdbfeaba28b8c228e1efa
Marc <marc.maninang@gmail.com>
2022-06-26 19:37:30 -0700

    added refactored map method solution

--- exercises/easy5/list_of_digits.js
+++ exercises/easy5/list_of_digits.js
@@ -7,6 +7,25 @@ function digitList(numbers) {
   return result;
 }
 
+/**
+ * Solution using .map() method
+ * 
+ */
+
+function digitList(numbers) {
+  let strNums = numbers.toString().split('');
+  const result = strNums.map( num => Number(num) );
+  return result;
+}
+
+/**
+ * Solution with refactored .map() method usage
+ */
+
+function digitList(numbers) {
+  return numbers.toString().split('').map(num => Number(num));
+}
+
 console.log(digitList(12345));       // [1, 2, 3, 4, 5]
 console.log(digitList(7));           // [7]
 console.log(digitList(375290));      // [3, 7, 5, 2, 9, 0]
