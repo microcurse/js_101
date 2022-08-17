09fb091780e145bc9af792324b84ee4284ad1ab5
Marc <marc.maninang@gmail.com>
2022-08-16 22:38:15 -0700

    started working on chapter 4. All essentially stuff I already knew because I went ahead on my exercises

---
+++ lesson_4/array_element_assignment.js
@@ -0,0 +1,6 @@
+let numbers = [1, 2, 3, 4];
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i] += 1;
+}
+numbers[4] + 2;
+console.log(numbers);
---
+++ lesson_4/object_key_assignment.js
@@ -0,0 +1,7 @@
+let obj = { apple: 'Produce', carrot: 'Produce', pear: 'Produce', broccoli: 'Produce' }
+obj['pear'] = 'Fruit'
+obj.broccoli = 'Vegetable'
+obj['apple'] = 'Fruit'
+obj.carrot = 'Vegetable'
+
+console.log(obj);
