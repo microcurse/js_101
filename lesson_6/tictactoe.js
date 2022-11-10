fd9883e47ee2f1ff1de2a94039b409bb82fe122f
Marc <marc.maninang@gmail.com>
2022-11-09 22:46:05 -0800

    Installed NPM packages on the root folder. Finished up with player choice input and checking for a proper input

--- lesson_6/tictactoe.js
+++ lesson_6/tictactoe.js
@@ -1,3 +1,5 @@
+const readline = require('../node_modules/readline-sync');
+
 function displayBoard(board) {
   console.log('');
   console.log('     |     |');
@@ -24,5 +26,31 @@ function initializeBoard() {
   return board;
 }
 
+function playerChoosesSquare(board) {
+  let square; // declared here so we can use it outside the loop
+
+  // valid square choices are those ' board' keays whose values are spaces
+  let emptySquares = Object.keys(board).filter(key => board[key] === ' ');
+
+  while (true) {
+    prompt('Choose a square (1-9)');
+    square = readline.question().trim(); // input trimmed to allow spaces in input
+    if (emptySquares.includes(square)) {
+      break;
+    } else {
+      prompt("Sorry, that's not a valid choice.");
+    }
+  }
+
+  board[square] = 'X';
+}
+
+function prompt(msg) {
+  console.log(`=> ${msg}`);
+}
+
 let board = initializeBoard();
+displayBoard(board);
+
+playerChoosesSquare(board);
 displayBoard(board);
